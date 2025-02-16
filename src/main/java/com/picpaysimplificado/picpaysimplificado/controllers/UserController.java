c89319bd255c284305b39444d5a577c3329b1515
Pablo Vinicios <pablo.araujo@alu.ufc.br>
2025-02-16 02:17:21 -0300

    refactor: adding the requestMapping

--- src/main/java/com/picpaysimplificado/picpaysimplificado/controllers/UserController.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/controllers/UserController.java
@@ -13,24 +13,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 
-@RestController("/users")
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
     
-    @PostMapping
+    @PostMapping()
     public ResponseEntity<User> createUser(@RequestBody UserDto user) {
         User newUser = userService.createUser(user);
         
         return new ResponseEntity<>(newUser,HttpStatus.CREATED);
     }
 
-    @GetMapping
+    @GetMapping()
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = this.userService.getAllUsers();
         return new ResponseEntity<>(users,HttpStatus.OK);
